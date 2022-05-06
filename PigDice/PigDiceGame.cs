367a212256be0945573cc971db40d9008114e7b2
Sarah Wainscott <swainscott@tql.com>
2022-05-06 15:39:00 -0400

    Add project files.

---
+++ PigDice/PigDiceGame.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigDice {
+    internal class PigDiceGame {
+
+        private Random rnd = new Random();
+
+        private int RollDie() {
+            return rnd.Next(1, 7);
+        }
+
+        public int Play() {
+            var score = 0;
+            var roll = RollDie();
+            while (roll != 1) {
+                score += roll;
+                roll= RollDie();
+            }
+            return score;
+        }
+    }
+
+}
---
+++ PigDice/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using PigDice;
+
+var piggie = new PigDiceGame();
+
+var highscore = 1000;
+long games = 0;
+var highest = 0;
+var score = piggie.Play();
+
+while (score < highscore) {
+    score = piggie.Play();
+    games++;
+    if (score > highest) {
+        highest = score;
+    }
+    if (games % 100000000 == 0) {
+        Console.WriteLine($"{games:n0} games played. High score: {highest}");
+    }
+}
+
